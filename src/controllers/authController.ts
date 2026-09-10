0b824458ba23e64683c111920a0a2d821c2a95d2
Deepak394 <dhimandeepak222@gmail.com>
2026-09-10 22:28:42 +0530

    response structure chnaged

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -3,39 +3,83 @@ import { registerUser, loginUser } from "../services/authService";
 import { registerSchema, loginSchema } from "../validation/authValidation";
 
 export async function register(req: Request, res: Response) {
-  const parsed:any = registerSchema.safeParse(req.body);
+  const parsed: any = registerSchema.safeParse(req.body);
   if (!parsed.success) {
-    return res.status(400).json({ error:
-       parsed.error.issues[0].message   
-        });
+    return res
+      .status(400)
+      .json({
+        success: false,
+        status: "error",
+        message: parsed.error.issues[0].message,
+      });
   }
 
   try {
     const user = await registerUser(parsed.data.email, parsed.data.password);
-    res.status(201).json({ user });
+    res
+      .status(201)
+      .json({
+        success: true,
+        status: "success",
+        message: "User registered successfully",
+        user,
+      });
   } catch (err) {
     if ((err as Error).message === "EMAIL_ALREADY_EXISTS") {
-      return res.status(409).json({ error: "Email already registered" });
+      return res
+        .status(409)
+        .json({
+          success: false,
+          status: "error",
+          message: "Email already registered",
+        });
     }
-    res.status(500).json({ error: "Something went wrong" });
+    res
+      .status(500)
+      .json({
+        success: false,
+        status: "error",
+        message: "Something went wrong",
+      });
   }
 }
 
 export async function login(req: Request, res: Response) {
-  const parsed:any = loginSchema.safeParse(req.body);
+  const parsed: any = loginSchema.safeParse(req.body);
   if (!parsed.success) {
-    return res.status(400).json({ error: 
-    parsed.error.issues[0].message
-        });
+    return res
+      .status(400)
+      .json({
+        success: false,
+        status: "error",
+        message: parsed.error.issues[0].message,
+      });
   }
 
   try {
     const result = await loginUser(parsed.data.email, parsed.data.password);
-    res.json(result);
+    res.json({
+      success: true,
+      status: "success",
+      message: "Login successful",
+      ...result,
+    });
   } catch (err) {
     if ((err as Error).message === "INVALID_CREDENTIALS") {
-      return res.status(401).json({ error: "Invalid email or password" });
+      return res
+        .status(401)
+        .json({
+          success: false,
+          status: "error",
+          message: "Invalid email or password",
+        });
     }
-    res.status(500).json({ error: "Something went wrong" });
+    res
+      .status(500)
+      .json({
+        success: false,
+        status: "error",
+        message: "Something went wrong",
+      });
   }
 }
